4db76f52624d1434eb7046cacc917677456afa7e
Christian Schuhmann <97274+madebyherzblut@users.noreply.github.com>
2022-12-03 09:35:12 +0000

    feat(2022): Add day 3

---
+++ 2022/03.mjs
@@ -0,0 +1,44 @@
+import fs from "node:fs/promises";
+
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size)
+  );
+
+const input = await fs.readFile("03_input.txt");
+const priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+{
+  const output = input
+    .toString()
+    .split("\n")
+    .reduce((acc, line) => {
+      const contents = line.split("");
+      const index = Math.ceil(contents.length / 2);
+      const compartments = [contents.splice(0, index), contents.splice(-index)];
+      const shared = compartments[0].find((i) => compartments[1].includes(i));
+
+      return acc + priority.indexOf(shared) + 1;
+    }, 0);
+
+  console.log("Part 1: ", output);
+}
+
+{
+  const output = chunk(
+    input
+      .toString()
+      .split("\n")
+      .filter(Boolean)
+      .map((l) => l.split("")),
+    3
+  ).reduce((acc, group) => {
+    console.log(group);
+    const shared = group[0].find(
+      (i) => group[1].includes(i) && group[2].includes(i)
+    );
+    return acc + priority.indexOf(shared) + 1;
+  }, 0);
+
+  console.log("Part 2: ", output);
+}
